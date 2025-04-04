fb39289cf4da6018e6d20490bb17e3bea15c7a02
jiya-0905 <138243313+jiya-0905@users.noreply.github.com>
2025-04-04 16:11:01 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from flask_cors import CORS
+import os
+
 
 app = Flask(__name__)
 CORS(app)
@@ -34,4 +36,6 @@ def analyze():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
